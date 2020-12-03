af08582313a9560a2415fea4b2bd1b038e40591e
PapaDrago <oscarwaii@hotmail.com>
2020-12-03 00:26:56 -0600

    jeje ahí más o menos ojalá se vea chido :v

---
+++ public/scripts/login.js
@@ -0,0 +1,16 @@
+jQuery('document').ready(function($){
+    var loginBtn=$('.btn_login'),
+        containerLogin=$('.containerLogin'),
+        whiteLogin=$('.containerWhite'),
+        blueLogin=$('.containerBlue'),
+        containerFooter=$('.containerFooter'),
+        info=$('.containerInfo');
+
+    loginBtn.click(function(){
+        containerLogin.addClass('containerLogin_closed');
+        whiteLogin.addClass('containerWhite_closed');
+        blueLogin.addClass('containerBlue_closed');
+        info.addClass('containerInfo_closed');
+        containerFooter.addClass('containerFooter_closed');
+    });
+});
