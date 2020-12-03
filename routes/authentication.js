2e5b9f2ec276e392c701bd2b6e533900ea620485
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-12-03 02:25:38 -0600

    Avances al sistema del login
    Ayuda no se que estoy haciendo con mi vida

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
 router.get('/login',(req,res,next) => {
     res.render('login', { title: 'Iniciar sesión' });
 });
 
-router.post('/login',(req,res) =>{
-    
+router.post('/login',(req, res, next) =>{
+    passport.authenticate('local.signin',{
+        successRedirect: '/inicio',
+        failureRedirect: '/login',
+        failureFlash: true
+    })(req, res, next);
 });
 
 module.exports = router;
