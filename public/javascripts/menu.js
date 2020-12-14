6fcc8f98d88ba873bf7b0ea4801ddeda69d75e3a
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-12-14 13:27:36 -0600

    Arreglo bugs script responsividad
    Ya arreglé algunos bugs y ya no es necesario estar recargando la pagina a cada rato

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,3 +1,4 @@
+var ancho;
 jQuery('document').ready(function($){
     var menuBtn=$('.main_icon'),
         left=$('.left_main'),
@@ -5,7 +6,7 @@ jQuery('document').ready(function($){
         footer=$('.footer_content'),
         container=$('.main_container'),
         dropdown=$('.categories_dropdown');
-    var ancho=$(window).width();
+    ancho=$(window).width();
     if(ancho<800){
         left.addClass('hide');
         top.addClass('hide');
@@ -46,5 +47,70 @@ jQuery('document').ready(function($){
                 container.addClass('mobile');
             }
         }
+    });
+    $(window).resize(function(){
+        var ancho=$(window).width();
+        if(ancho<800){
+            left.addClass('hide');
+            top.addClass('hide');
+            footer.addClass('hide');
+            dropdown.addClass('hide');
+            container.addClass('hide');
+
+            left.removeClass('mobile');
+            top.removeClass('mobile');
+            footer.removeClass('mobile');
+            dropdown.removeClass('mobile');
+            container.removeClass('mobile');
+        }
+        else{
+            left.removeClass('hide');
+            top.removeClass('hide');
+            footer.removeClass('hide');
+            dropdown.removeClass('hide');
+            container.removeClass('hide');
+
+            left.removeClass('mobile');
+            top.removeClass('mobile');
+            footer.removeClass('mobile');
+            dropdown.removeClass('mobile');
+            container.removeClass('mobile');
+        }
+        ancho=$(window).width();
+        menuBtn.unbind('click');
+        menuBtn.click(function(){
+            if(ancho>=800){
+                if(left.hasClass('hide')){
+                    left.removeClass('hide');
+                    top.removeClass('hide');
+                    footer.removeClass('hide');
+                    dropdown.removeClass('hide');
+                    container.removeClass('hide');
+                }
+                else{
+                    left.addClass('hide');
+                    top.addClass('hide');
+                    footer.addClass('hide');
+                    dropdown.addClass('hide');
+                    container.addClass('hide');
+                }
+            }
+            else{
+                if(left.hasClass('mobile')){
+                    left.removeClass('mobile');
+                    top.removeClass('mobile');
+                    footer.removeClass('mobile');
+                    dropdown.removeClass('mobile');
+                    container.removeClass('mobile');
+                }
+                else{
+                    left.addClass('mobile');
+                    top.addClass('mobile');
+                    footer.addClass('mobile');
+                    dropdown.addClass('mobile');
+                    container.addClass('mobile');
+                }
+            }
+        });
     });
 });
