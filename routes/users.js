a53a12c83c6a0b51d813e64aa14f312c524d70ca
leoDlopez <lopez_lopez_daniel@hotmail.com>
2020-12-16 13:23:30 -0600

    pusshing
    Hola

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { check, validationResult } = require('express-validator/check');
-const {isLoggedIn , isNotLoggedIn} = require('../lib/auth');
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
 //autenticacion
 const passport = require('passport');
 
@@ -10,15 +10,20 @@ const models = require('../models/index');
 
 /* GET users listing. */
 //TODOS LOS USUARIOS
-router.get('/', isLoggedIn,function(req, res, next) {
-  models.User.findAll()
-  .then(usuarios => {
-    res.send(usuarios)
-  });
+/*router.get('/', isLoggedIn, function (req, res, next) {
+    models.User.findAll({
+        include: ["employee"],
+    }).then(usuarios => {
+        res.send(usuarios)
+    });
+});*/
+
+router.get('/', isLoggedIn, function (req, res, next) {
+    res.render('usuarios')
 });
 
 //AGREGAR USUARIO
-router.get('/nuevo', isLoggedIn,function(req, res, next) {
+router.get('/nuevo', isLoggedIn, function (req, res, next) {
     res.render('nuevoUsuario')
 });
 
@@ -26,66 +31,68 @@ router.post(
     '/nuevo',
     //validacion backend
     [
-      check('email')
-      .isEmail()
-      .normalizeEmail(),
-      check('password')
-      .isLength({ min:6 })
+        check('email')
+            .isEmail()
+            .normalizeEmail(),
+        check('password')
+            .isLength({ min: 6 })
     ],
     isLoggedIn,
-    function(req, res, next) {
-      //maneja los errores de la validacion
-      const errors = validationResult(req);
-      if(!errors.isEmpty()){
-          console.log(errors);
-          return res.status(422).json({ errors:errors.array() });
-      }
+    function (req, res, next) {
+        //maneja los errores de la validacion
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors);
+            return res.status(422).json({ errors: errors.array() });
+        }
 
-      //autenticacion e insercion en la bd
-      passport.authenticate('local.signup',{
-        successRedirect: '/usuarios',
-        failureRedirect: '/inicio',
-        failureFlash: true,
-        session: false
-      })(req, res, next);
+        //autenticacion e insercion en la bd
+        passport.authenticate('local.signup', {
+            successRedirect: '/usuarios',
+            failureRedirect: '/inicio',
+            failureFlash: true,
+            session: false
+        })(req, res, next);
     }
 );
 
 //VER USUARIO ID
-router.get('/:id', isLoggedIn, function(req, res, next) {
-  let id = req.params.id
-  models.User
-  .findOne({ 
-    where: {id: id}})
-  .then(user => {
-      res.send(user)
-    })
+router.get('/:id', isLoggedIn, function (req, res, next) {
+    let id = req.params.id
+    models.User
+        .findOne({
+            where: { id: id }
+        })
+        .then(user => {
+            res.send(user)
+        })
 });
 
 //UPDATE USUARIO ID
-router.put('/:id', isLoggedIn, function(req, res, next) {
-  let id = req.params.id
-  let nuevosDatos = req.body
-  models.User
-  .findOne({ 
-    where: {id: id}})
-  .then(user => {
-      user.update(nuevosDatos)
-      .then(newUser => {
-        res.send(newUser)
-      })
-    })
+router.put('/:id', isLoggedIn, function (req, res, next) {
+    let id = req.params.id
+    let nuevosDatos = req.body
+    models.User
+        .findOne({
+            where: { id: id }
+        })
+        .then(user => {
+            user.update(nuevosDatos)
+                .then(newUser => {
+                    res.send(newUser)
+                })
+        })
 });
 
 //ELIMINAR USUARIO ID
-router.delete('/:id', isLoggedIn, function(req, res, next) {
-  let id = req.params.id
+router.delete('/:id', isLoggedIn, function (req, res, next) {
+    let id = req.params.id
 
-  models.User.destroy({
-    where: {id: id}
-  }).then(() => {
-    res.send('Persona eliminada')
-  })
+    models.User.destroy({
+        where: { id: id }
+    }).then(() => {
+        res.send('Persona eliminada')
+    })
 });
 
 module.exports = router;
