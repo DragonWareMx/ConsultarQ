a52317b67061f5701bf47b764d019c52da25b0fe
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-01-06 23:59:33 -0600

    Ya esta el update y el delete
    Falta el ajax

--- routes/roles.js
+++ routes/roles.js
@@ -44,4 +44,34 @@ router.post('/create', isLoggedIn, async function (req, res, next) {
     })
 });
 
+router.post('/update/:id', isLoggedIn, async function (req, res, next) {
+    var permsID = []
+    for (var key in req.body) {
+        if (key != "role_name") {
+            const permission = await models.Permission.findOne({ where: { name: key }, raw: true });
+            permsID.push(permission['id'])
+        }
+    }
+    //se busca el rol
+    models.Role.findOne({
+        where: { id: req.params.id }
+    }).then(async (rol) => {
+        console.log(rol)
+        await rol.setPermissions(permsID)
+        res.send("si jaló");
+    })
+});
+
+router.post('/delete/:id', isLoggedIn, async function (req, res, next) {
+    //se elimina el rol
+    let role = await models.Role.findOne({ where: { id: req.params.id } }).catch(e => {
+        console.log(e.message)
+    })
+    if (!role) {
+        console.log("err");
+    }
+    role.destroy();
+    res.send("si se pudo coch")
+});
+
 module.exports = router;
