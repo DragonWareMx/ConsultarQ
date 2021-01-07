4303463c367dfaf0ee481855e20203ede7069c54
FernandoGASA <66817120+FernandoGASA@users.noreply.github.com>
2021-01-07 00:35:00 -0600

    creado rol dragonware

--- seeders/20201217033717-roles.js
+++ seeders/20201217033717-roles.js
@@ -13,6 +13,9 @@ module.exports = {
     */
     await queryInterface.bulkInsert('Roles', [
       {
+        name: 'DragonWare'
+      },
+      {
         name: 'Administrador'
       },
       {
@@ -22,20 +25,116 @@ module.exports = {
 
     await queryInterface.bulkInsert('permission_role', [
       {
+        Roleid: 1,
+        PermissionId: 1
+      },
+      {
+        Roleid: 1,
+        PermissionId: 2
+      },
+      {
+        Roleid: 1,
+        PermissionId: 3
+      },
+      {
+        Roleid: 1,
+        PermissionId: 4
+      },
+      {
+        Roleid: 1,
+        PermissionId: 5
+      },
+      {
+        Roleid: 1,
+        PermissionId: 6
+      },
+      {
+        Roleid: 1,
+        PermissionId: 7
+      },
+      {
+        Roleid: 1,
+        PermissionId: 8
+      },
+      {
+        Roleid: 1,
+        PermissionId: 9
+      },
+      {
+        Roleid: 1,
+        PermissionId: 10
+      },
+      {
+        Roleid: 1,
+        PermissionId: 11
+      },
+      {
+        Roleid: 1,
+        PermissionId: 12
+      },
+      {
+        Roleid: 1,
+        PermissionId: 13
+      },
+      {
+        Roleid: 1,
+        PermissionId: 14
+      },
+      {
+        Roleid: 1,
+        PermissionId: 15
+      },
+      {
+        Roleid: 1,
+        PermissionId: 16
+      },
+      {
+        Roleid: 1,
+        PermissionId: 17
+      },
+      {
+        Roleid: 1,
+        PermissionId: 18
+      },
+      {
+        Roleid: 1,
+        PermissionId: 19
+      },
+      {
+        Roleid: 1,
+        PermissionId: 20
+      },
+      {
+        Roleid: 1,
+        PermissionId: 21
+      },
+      {
+        Roleid: 1,
+        PermissionId: 22
+      },
+      {
+        Roleid: 1,
+        PermissionId: 23
+      },
+      {
+        Roleid: 1,
+        PermissionId: 24
+      },
+      {
         PermissionId: 1,
-        Roleid: 1
+        Roleid: 2
       },
       {
         PermissionId: 2,
-        Roleid: 1
+        Roleid: 2
       },
       {
         PermissionId: 3,
-        Roleid: 1
+        Roleid: 2
       },
       {
         PermissionId: 4,
-        Roleid: 1
+        Roleid: 2
       }
     ], {});
   },
