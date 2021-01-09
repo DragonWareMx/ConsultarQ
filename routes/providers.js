ff4fe831d8bc9e4fc32038e28d56e7ae68cd2f5d
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-01-09 16:31:10 -0600

    Trabajo en progreso
    Es poco pero es trabajo honesto

--- routes/providers.js
+++ routes/providers.js
@@ -9,7 +9,7 @@ const models = require('../models/index');
 //  consulta todos los prestadores externos
 router.get('/', isLoggedIn, async (req, res, next) => {
 
-    try{
+    try {
         //obtenemos el usuario, su rol y su permiso
         let usuario = await models.User.findOne({
             where: {
@@ -22,12 +22,12 @@ router.get('/', isLoggedIn, async (req, res, next) => {
                 model: models.Role,
                 include: {
                     model: models.Permission,
-                    where: {name: 'ur'}
+                    where: { name: 'ur' }
                 }
             }
         })
 
-        if(usuario && usuario.Role && usuario.Role.Permissions){
+        if (usuario && usuario.Role && usuario.Role.Permissions) {
             //TIENE PERMISO DE DESPLEGAR VISTA
             models.Provider.findAll({
                 include: [{
@@ -38,16 +38,16 @@ router.get('/', isLoggedIn, async (req, res, next) => {
                 ]
             }).then(prestadores => {
                 models.Provider.findAll().then(areas => {
-                    return res.render('prestadores_externos/prestadores', { prestadores , areas})
+                    return res.render('prestadores_externos/prestadores', { prestadores, areas })
                 });
             });
         }
-        else{
+        else {
             //NO TIENE PERMISOS
             return res.status(403).json(403)
         }
     }
-    catch(error){
+    catch (error) {
         return res.status(403).json(403)
     }
 
@@ -56,25 +56,60 @@ router.get('/', isLoggedIn, async (req, res, next) => {
 // agregar nuevo prestador
 router.post('/nuevo', isLoggedIn,
     [
+        check('add-nombre')
+            .not().isEmpty().withMessage('Nombre es un campo requerido.')
+            .isLength({ max: 255 }).withMessage('El nombre puede tener un máximo de 255 caracteres.')
+            .trim()
+            .escape(),
         check('add-correo')
-            .isEmail()
-            .normalizeEmail().withMessage('Correo electrónico no válido.'),
-    ]
-    , (req, res, next) => {
+            .not().isEmpty().withMessage('Correo electrónico es un campo requerido.')
+            .isEmail().withMessage('Correo electrónico no válido.')
+            .normalizeEmail(),
+        check('add-area')
+            .optional({ checkFalsy: false })
+            .custom(async (area) => {
+                var validador = false
+                //si no es nulo
+                if (area) {
+                    validador = false
+
+                    let ids = await models.Provider_Area.findAll({
+                        attributes: ['id'],
+                        raw: true
+                    })
+
+                    ids.forEach(id => {
+                        if (area == id.id) {
+                            validador = true
+                        }
+                    });
+                }
+                if (validador) {
+                    return true
+                }
+                else
+                    throw new Error('El área no existe.');
+            }).withMessage('El área no es válido.'),
+        check('add-telefono')
+            .not().isEmpty().withMessage('Número telefónico es un campo requerido.')
+            .trim()
+            .isNumeric().withMessage('Sólo se aceptan números en el número telefónico.')
+            .isLength({ max: 50, min: 10 }).withMessage('El número telefónico debe tener al menos 10 dígitos.'),
+        check('add-dro')
+            .not().isEmpty().withMessage('DRO es un campo requerido.')
+            .isLength({ max: 255 }).withMessage('Ciudad puede tener un máximo de 255 caracteres.')
+            .trim()
+            .escape(),
+        check('add-estatus').isIn(['active', 'inactive']).withMessage('El estatus ingresado no es válido.')
+    ],
+
+    async (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).send(errors.array());
         }
-        passport.authenticate('local.signup', function (error, user, info) {
-            if (error) {
-                return res.status(500).json([{ msg: 'Ocurrió un error al intentar registrar el prestador.' }]);
-            }
-            if (!user) {
-                return res.status(401).json(info);
-            }
-            res.json(user);
-        })(req, res, next);
-});
+        res.send(req.body);
+    });
 
 
 module.exports = router;
