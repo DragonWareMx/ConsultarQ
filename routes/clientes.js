931273bb41a2b4d645117168fd2f12a889b98c1b
AdolfoLem <74695413+AdolfoLem@users.noreply.github.com>
2021-01-17 00:45:42 -0600

    areas de prestadores y clientes

--- routes/clientes.js
+++ routes/clientes.js
@@ -536,8 +536,8 @@ router.get('/generar-cotizacion', isLoggedIn,function(req, res, next) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//  AREAS DE LOS PRESTADORES EXTERNOS
-//  consulta todas las areas de los prestadores externos
+//  AREAS DE LOS CLIENTES
+//  consulta todas las areas de los clientes
 router.get('/areas', isLoggedIn, async (req, res, next) => {
 
     try{
@@ -581,60 +581,27 @@ router.get('/areas', isLoggedIn, async (req, res, next) => {
 
 });
 
-//AGREAGAR AREA
+//  AGREGAR AREA
 router.post('/areas/nuevo',
     [
-        check('add_nombre')
+        check('addNombre')
             .not().isEmpty().withMessage('Nombre es un campo requerido.')
             .isLength({ max: 255 }).withMessage('El nombre puede tener un máximo de 255 caracteres.')
             .trim()
             .escape(),
-        check('add_correo')
-            .not().isEmpty().withMessage('Correo electrónico es un campo requerido.')
-            .isEmail().withMessage('Correo electrónico no válido.')
-            .normalizeEmail(),
-        check('add_area')
-            .optional({ checkFalsy: false })
-            .custom(async (area) => {
-                var validador = false
-                //si no es nulo
-                if (area) {
-                    validador = false
-
-                    let ids = await models.Client_Area.findAll({
-                        attributes: ['id'],
-                        raw: true
-                    })
-
-                    ids.forEach(id => {
-                        if (area == id.id) {
-                            validador = true
-                        }
-                    });
-                }
-                if (validador) {
-                    return true
-                }
-                else
-                    throw new Error('El área no existe.');
-            }).withMessage('El área no es válido.'),
-        check('add_telefono')
-            .not().isEmpty().withMessage('Número telefónico es un campo requerido.')
-            .trim()
-            .isNumeric().withMessage('Sólo se aceptan números en el número telefónico.')
-            .isLength({ max: 50, min: 10 }).withMessage('El número telefónico debe tener al menos 10 dígitos.'),
-        check('add_dro')
-            .not().isEmpty().withMessage('DRO es un campo requerido.')
-            .isLength({ max: 255 }).withMessage('Ciudad puede tener un máximo de 255 caracteres.')
+        check('addDescripcion')
+            .not().isEmpty().withMessage('Descripción es un campo requerido.')
+            .isLength({ max: 255 }).withMessage('La descripción puede tener un máximo de 255 caracteres.')
             .trim()
             .escape(),
-        check('add_estatus').isIn(['active', 'inactive']).withMessage('El estatus ingresado no es válido.')
     ],
-    async (req, res, next) => {
+    isLoggedIn, async function (req, res, next) {
+        //si hay errores entonces se muestran
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).send(errors.array());
         }
+
         try {
             //VERIFICACION DEL PERMISO
 
@@ -647,32 +614,45 @@ router.post('/areas/nuevo',
                     model: models.Role,
                     include: {
                         model: models.Permission,
-                        where: { name: 'ec' }
+                        where: { name: 'uc' }           //////////////////  cambiar nombre del permiso 
                     }
                 }
             })
 
             if (!(usuario && usuario.Role && usuario.Role.Permissions)) {
-                //NO TIENE PERMISO DE AGREGAR rol
-                return res.status(403).json([{ msg: 'No estás autorizado para registrar prestadores.' }])
+                //NO TIENE PERMISO DE AGREGAR EL CONCEPTO
+                return res.status(403).json([{ msg: 'No estás autorizado para registrar áreas.' }])
+
+                //console.log(req.addNombre);
+                //res.status(200).json([{ status: 200 }]);
             }
         }
         catch (error) {
-            return res.status(403).json([{ msg: 'No estás autorizado para registrar prestadores.' }])
+            return res.status(403).json([{ msg: 'No fue posible registrar el área, inténtelo más tarde.' }])
         }
-        //aqui va la transaccion pero no se como hacerla xdd
+
+        //TIENE PERMISO
+        //Transaccion
         const t = await models.sequelize.transaction()
         try {
-            //GUARDA EL PRESTADOR
-            //se crea el prestador
-            const newPrestador = await models.Provider.create({
-                name: req.body.add_nombre,
-                dro: req.body.add_dro,
-                email: req.body.add_correo,
-                phone_number: req.body.add_telefono,
-                ProviderAreaId: req.body.add_area,
-                status: req.body.add_estatus,
+            //GUARDA EL CONCEPTO
+            //guarda el CONCEPTO
+            //var permsID = []
+            //permsID.push(permission['id'])
+            //console.log(permsID);
+            // for (var key in req.body) {
+            //     if (key != "role_name") {
+            //         const permission = await models.Permission.findOne({ where: { name: key }, raw: true, transaction: t });
+            //         permsID.push(permission['id'])
+            //     }
+            // }
+            //se crea el area
+            const newArea = await models.Client_Area.create({
+                name: req.body.addNombre,
+                description: req.body.addDescripcion,
             }, { transaction: t })
+
+            //await newArea.setPermissions(permsID, { transaction: t })
 
             //SE REGISTRA EL LOG
             //obtenemos el usuario que realiza la transaccion
@@ -684,22 +664,34 @@ router.post('/areas/nuevo',
             })
 
             //descripcion del log
-            var desc = "El usuario " + usuario.email + " ha registrado un prestador nuevo con los siguientes datos:\nNombre: " + newPrestador.name + "\nDRO: " + newPrestador.dro + "\nNúmero telefónico: " + newPrestador.phone_number + "\nEmail: " + newPrestador.email + "\nArea: " + newPrestador.ProviderAreaId + "\nStatus: " + newPrestador.status
+            var desc = "El usuario " + usuario.email + " ha registrado un área de clientes nueva con los siguientes datos:\nNombre: " + newArea.name + "\nDescripción: " + newArea.description +"\nCon los permisos:\n"
+
+            // var contador = 0
+            // for (var key in req.body) {
+            //     if (key != "role_name") {
+            //         desc = desc + key + "\n"
+            //         contador++
+            //     }
+            // }
+
+            // if (contador == 0) {
+            //     desc = desc + "Ningún permiso"
+            // }
 
             //guardamos los datos del log
             var dataLog = {
                 UserId: usuario.id,
-                title: "Registro de prestador",
+                title: "Registro de área de cliente",
                 description: desc
             }
 
             //guarda el log en la base de datos
             const log = await models.Log.create(dataLog, { transaction: t })
 
-            //verifica que se hayan registrado el log y el prestador
+            //verifica que se hayan registrado el log y el rol
             if (!log)
                 throw new Error()
-            if (!newPrestador)
+            if (!newArea)
                 throw new Error()
 
             res.status(200).json([{ status: 200 }]);
@@ -711,9 +703,221 @@ router.post('/areas/nuevo',
             // If the execution reaches this line, an error was thrown.
             // We rollback the transaction.
             await t.rollback();
-            return res.status(500).json([{ msg: 'No fue posible registrar el prestador, vuelva a intentarlo más tarde.' }])
+            return res.status(500).json([{ msg: 'No fue posible registrar el área, vuelva a intentarlo más tarde.' }])
         }
-    });
+        
+    }
+);
+
+//  ACTUALIZAR AREA
+router.post('/areas/update/:id',
+    [
+        check('editNombre')
+            .not().isEmpty().withMessage('Nombre es un campo requerido.')
+            .isLength({ max: 255 }).withMessage('El nombre puede tener un máximo de 255 caracteres.')
+            .trim()
+            .escape(),
+        check('editDescripcion')
+            .not().isEmpty().withMessage('Descripción es un campo requerido.')
+            .isLength({ max: 255 }).withMessage('La descripción puede tener un máximo de 255 caracteres.')
+            .trim()
+            .escape(),
+    ]
+    , isLoggedIn, async function (req, res, next) {
+        //si hay errores entonces se muestran
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).send(errors.array());
+        }
+
+        try {
+            //VERIFICACION DEL PERMISO
+
+            //obtenemos el usuario, su rol y su permiso
+            let usuario = await models.User.findOne({
+                where: {
+                    id: req.user.id
+                },
+                include: {
+                    model: models.Role,
+                    include: {
+                        model: models.Permission,
+                        where: { name: 'uu' }           ////////////////////////REVISAR PERMISO
+                    }
+                }
+            })
+
+            if (!(usuario && usuario.Role && usuario.Role.Permissions)) {
+                //NO TIENE PERMISO DE AGREGAR areas
+                return res.status(403).json([{ msg: 'No estás autorizado para actualizar áreas.' }])
+            }
+        }
+        catch (error) {
+            return res.status(403).json([{ msg: 'No estás autorizado para actualizar áreas.' }])
+        }
+
+        //TIENE PERMISO
+        //Transaccion
+        const t = await models.sequelize.transaction()
+        try {
+            //ACTUALIZA EL ÁREA
+            //verificamos que no exista un concepto con el mismo nombre
+            const areaV = await models.Concept.findOne({ where: { name: req.body.editNombre, id: { $not: req.params.id } }, transaction: t })
+            if (areaV) {
+                await t.rollback();
+                return res.status(500).json([{ msg: 'Ya existe un área con el mismo nombre.' }])
+            }
+            else {
+                // var permsID = []
+                // for (var key in req.body) {
+                //     if (key != "role_name") {
+                //         const permission = await models.Permission.findOne({ where: { name: key }, raw: true, transaction: t });
+                //         permsID.push(permission['id'])
+                //     }
+                // }
+                //se actualiza el area
+                const areaU = await models.Client_Area.findOne({
+                    where: { id: req.params.id }, transaction: t
+                })
+
+                await areaU.update({
+                    name: req.body.editNombre,
+                    description: req.body.editDescripcion
+                }, { transaction: t })
+
+                //await areaU.setPermissions(permsID, { transaction: t })
+
+                //SE REGISTRA EL LOG
+                //obtenemos el usuario que realiza la transaccion
+                const usuario = await models.User.findOne({
+                    where: {
+                        id: req.user.id
+                    },
+                    transaction: t
+                })
+
+                //descripcion del log
+                var desc = "El usuario " + usuario.email + " ha actualizado el área de clientes con el id " + areaU.id + " con los siguientes datos:\nNombre: " + areaU.name + "\nDescripción: " + areaU.editDescripcion + "\nCon los permisos:\n"
+
+                // var contador = 0
+                // for (var key in req.body) {
+                //     if (key != "role_name") {
+                //         desc = desc + key + "\n"
+                //         contador++
+                //     }
+                // }
+
+                // if (contador == 0) {
+                //     desc = desc + "Ningún permiso"
+                // }
+
+                //guardamos los datos del log
+                var dataLog = {
+                    UserId: usuario.id,
+                    title: "Actualización de área de cliente",
+                    description: desc
+                }
+
+                //guarda el log en la base de datos
+                const log = await models.Log.create(dataLog, { transaction: t })
+
+                //verifica que se hayan registrado el log y el rol
+                if (!log)
+                    throw new Error()
+                if (!areaU)
+                    throw new Error()
+
+                res.status(200).json([{ status: 200 }]);
+                // If the execution reaches this line, no errors were thrown.
+                // We commit the transaction.
+                await t.commit()
+            }
+        } catch (error) {
+
+            // If the execution reaches this line, an error was thrown.
+            // We rollback the transaction.
+            await t.rollback();
+            return res.status(500).json([{ msg: 'No fue posible actualizar el área, vuelva a intentarlo más tarde.' }])
+        }
+});
+
+//  ELIMINAR AREA
+router.post('/areas/delete/:id', isLoggedIn, async function (req, res, next) {
+    try {
+        //VERIFICACION DEL PERMISO
+
+        //obtenemos el usuario, su rol y su permiso
+        let usuario = await models.User.findOne({
+            where: {
+                id: req.user.id
+            },
+            include: {
+                model: models.Role,
+                include: {
+                    model: models.Permission,
+                    where: { name: 'ud' }               ///////////////////////////VERIFICAR QUE SEA EL PERMISO CORRECTO
+                }
+            }
+        })
+
+        if (!(usuario && usuario.Role && usuario.Role.Permissions)) {
+            //NO TIENE PERMISOS
+            return res.status(403).json([{ msg: 'No tienes permiso de eliminar áreas.' }])
+        }
+    }
+    catch (error) {
+        return res.status(403).json([{ msg: 'No tienes permiso de eliminar áreas.' }])
+    }
+
+    //TIENE PERMISO
+
+    //Transaccion
+    const t = await models.sequelize.transaction()
+    try {
+        //se elimina el rol
+        const areaD = await models.Client_Area.findOne({ where: { id: req.params.id }, transaction: t })
+
+        await areaD.destroy({ transaction: t });
+
+        //SE REGISTRA EL LOG
+        //obtenemos el usuario que realiza la transaccion
+        const usuario = await models.User.findOne({
+            where: {
+                id: req.user.id
+            },
+            transaction: t
+        })
+
+        //guardamos los datos del log
+        var dataLog = {
+            UserId: usuario.id,
+            title: "Eliminación de área de prestador externo",
+            description: "El usuario " + usuario.email + " ha eliminado el área de clientes con el id " + areaD.id + " con los siguientes datos: \nNombre " + areaD.name + "\nDescripción " + areaD.description
+        }
+
+        //guarda el log en la base de datos
+        const log = await models.Log.create(dataLog, { transaction: t })
+
+        //verifica si se elimina el concepto
+        const verArea = await models.Client_Area.findOne({ where: { id: areaD.id }, transaction: t })
+
+        if (verArea)
+            throw new Error()
+        if (!log)
+            throw new Error()
+
+        res.status(200).json([{ status: 200 }]);
+        // If the execution reaches this line, no errors were thrown.
+        // We commit the transaction.
+        await t.commit()
+    } catch (error) {
+
+        // If the execution reaches this line, an error was thrown.
+        // We rollback the transaction.
+        await t.rollback();
+        return res.status(500).json([{ msg: 'No fue posible eliminar el área, vuelva a intentarlo más tarde.' }])
+    }
+});
 
 
 
