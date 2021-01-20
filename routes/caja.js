3f8627d7ff5ce9bbcda3b16ca7c237e3dcadf548
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-01-19 20:19:16 -0600

    Ya está el edit y arregle unos cuantos detalles más
    hola

--- routes/caja.js
+++ routes/caja.js
@@ -119,7 +119,97 @@ router.post('/editar-registro/:id', isLoggedIn,
             .escape(),
     ],
     async function (req, res, next) {
-        res.send(req.body);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).send(errors.array());
+        }
+        try {
+            //VERIFICACION DEL PERMISO
+
+            //obtenemos el usuario, su rol y su permiso
+            let usuario = await models.User.findOne({
+                where: {
+                    id: req.user.id
+                },
+                include: {
+                    model: models.Role,
+                    include: {
+                        model: models.Permission,
+                        where: { name: 'cu' }
+                    }
+                }
+            })
+
+            if (!(usuario && usuario.Role && usuario.Role.Permissions)) {
+                //NO TIENE PERMISO DE AGREGAR rol
+                return res.status(403).json([{ msg: 'No estás autorizado para registrar transacciones.' }])
+            }
+        }
+        catch (error) {
+            return res.status(403).json([{ msg: 'No estás autorizado para registrar transacciones.' }])
+        }
+        //aqui va la transaccion pero no se como hacerla xdd
+        const t = await models.sequelize.transaction()
+        try {
+            //GUARDA LA TRANSACCION\
+            //se busca la transaccion que se va a actualizar
+            const oldTransaccion = await models.Transaction.findOne({ where: { id: req.params.id }, include: { model: models.Project_Employee } }, { transaction: t })
+            if (!oldTransaccion)
+                throw new Error()
+            //se busca a que proyecto del usuario se le va a asignar ahora la transaccion
+            const project_employ = await models.Project_Employee.findOne({ where: { ProjectId: req.body.proyecto, UserId: oldTransaccion.Project_Employee.UserId } }, { transaction: t })
+            if (!project_employ)
+                throw new Error()
+            //se actualiza la transaccion
+            await oldTransaccion.update({
+                T_type: req.body.type,
+                date: req.body.date,
+                amount: req.body.monto,
+                description: req.body.descripcion,
+                invoice: req.body.invoice,
+                ProjectEmployeeId: project_employ.id,
+                PaTypeId: req.body.pago,
+                ConceptId: req.body.concepto,
+            }, { transaction: t })
+
+            //SE REGISTRA EL LOG
+            //obtenemos el usuario que realiza la transaccion
+            const usuario = await models.User.findOne({
+                where: {
+                    id: req.user.id
+                },
+                transaction: t
+            })
+
+            //descripcion del log
+            var desc = "El usuario " + usuario.email + " ha actualizado una transaccion con los siguientes datos: \nId: " + oldTransaccion.id + "\nTipo: " + oldTransaccion.T_type + "\nFecha: " + oldTransaccion.date + "\nMonto: " + oldTransaccion.amount + "\nDescripcion: " + oldTransaccion.description + "\nDeducible: " + oldTransaccion.invoice
+
+            //guardamos los datos del log
+            var dataLog = {
+                UserId: usuario.id,
+                title: "Actualización de transacción",
+                description: desc
+            }
+
+            //guarda el log en la base de datos
+            const log = await models.Log.create(dataLog, { transaction: t })
+
+            //verifica que se hayan registrado el log y el prestador
+            if (!log)
+                throw new Error()
+            if (!oldTransaccion)
+                throw new Error()
+
+            res.status(200).json([{ status: 200 }]);
+            // If the execution reaches this line, no errors were thrown.
+            // We commit the transaction.
+            await t.commit()
+        } catch (error) {
+            // If the execution reaches this line, an error was thrown.
+            // We rollback the transaction.
+            await t.rollback();
+            return res.status(500).json([{ msg: 'No fue posible registrar el cambio en la transacción, vuelva a intentarlo más tarde.' }])
+        }
     })
 
 //  se consultan solo los egresos
@@ -300,7 +390,7 @@ router.get('/ingresos', isLoggedIn, async (req, res, next) => {
                         ['date', 'DESC']
                     ],
                     where: {
-                        T_type: 'egreso'
+                        T_type: 'ingreso'
                     }
                 })
             }
@@ -411,7 +501,7 @@ router.get('/deducibles', isLoggedIn, async (req, res, next) => {
                         ['date', 'DESC']
                     ],
                     where: {
-                        T_type: 'egreso'
+                        invoice: 1
                     }
                 })
             }
@@ -668,7 +758,6 @@ router.post('/create',
             // We commit the transaction.
             await t.commit()
         } catch (error) {
-
             // If the execution reaches this line, an error was thrown.
             // We rollback the transaction.
             await t.rollback();
